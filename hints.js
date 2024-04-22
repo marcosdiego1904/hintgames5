d604fc9db48a7a4edf6d343e929dca7645a9b4e3
marcos <diegopalomeque1904@gmail.com>
2024-04-21 22:09:08 -0400

    third

---
+++ hints.js
@@ -0,0 +1,110 @@
+let moses = [
+    {
+        'hint':'Hint#1',
+        'text': 'He was a leader of the People of Israel'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'He spoke directly to God'
+    },
+    {
+        'hint': 'Hint#3',
+        'text': 'He spoke with Elijah and Jesus. '
+    },
+    {
+        'hint': 'Hint#4',
+        'text': 'The 10 Commandments were given to him'
+    }
+]
+
+let peter = [
+    {
+        'hint':'Hint#1',
+        'text': 'He lived in the time of Jesus'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'His brother introduced him to Jesus'
+    },
+    {
+        'hint': 'Hint#3',
+        'text': 'Jesus called him the rock of the church'
+    },
+    {
+        'hint': 'Hint#4',
+        'text': 'He denied three times that he knew Jesus'
+    }
+]
+
+let paul = [
+    {
+        'hint': 'Hint#1',
+        'text': 'Preached his Faith before important Roman rulers'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'He was present at the death of Esteban'
+    },
+    {
+        'hint':'Hint#3',
+        'text': 'He persecuted the Church'
+    },
+    {
+        'hint':'Hint#4',
+        'text': 'He was bitten by a snake'
+    }
+]
+let john = [
+    {
+        'hint': 'Hint#1',
+        'text': 'His brother was James'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'He wrote 5 books of the Bible'
+    },
+    {
+        'hint':'Hint#3',
+        'text': 'Jesus revealed to him the book of Revelation'
+    },
+    {
+        'hint':'Hint#4',
+        'text': 'He was the beloved disciple of Jesus'
+    }
+]
+let joshua = [
+    {
+        'hint': 'Hint#1',
+        'text': 'He was the son of Nun'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'He was under the orders of Moses'
+    },
+    {
+        'hint':'Hint#3',
+        'text': 'He led the miracle of the walls of Jericho.'
+    },
+    {
+        'hint':'Hint#4',
+        'text': 'He was one of the 12 spies sent by moses'
+    }
+]
+let david = [
+    {
+        'hint': 'Hint#1',
+        'text': 'played the harp very well'
+    },
+    {
+        'hint' : 'Hint#2',
+        'text': 'He was the youngest of 8 siblings'
+    },
+    {
+        'hint':'Hint#3',
+        'text': 'He was anointed to be King'
+    },
+    {
+        'hint':'Hint#4',
+        'text': `It is part of the messiah's genealogy.`
+    }
+]
---
+++ index2.js
@@ -0,0 +1,132 @@
+let yes = document.querySelector("#yes");
+let no = document.querySelector("#no")
+function firstQuiz(test,answer,test2,answer2){
+  $(".leave").hide();
+    $(".input").hide();
+    $(".inputs2").hide();
+    $(".hintexts").text(test[0].text);
+    $(".hintn").text(test[0].hint)
+    var currentHintIndex = 1; 
+    currentHintIndex
+    $(".next").click(function() {
+      if (currentHintIndex < test.length) {
+        $(".hintexts").text(test[currentHintIndex].text);
+        $(".hintn").text(test[currentHintIndex].hint); 
+        currentHintIndex++; 
+      } else {
+       
+        $(".hintexts").text("No more hints available.");
+      }
+    });
+    let submitBut = document.querySelector(".subBut");
+    submitBut.addEventListener("click",function(){
+        let AnswerValue = $(".inputAnswer").val();
+        let validation = AnswerValue.toLowerCase();
+        console.log(validation)
+        $(".inputAnswer").val("")
+        if(validation == answer){
+            $(".showans2").addClass("showans")
+            $(".dec").text("Nice Job!")
+            $("#goback").hide();
+            $(".input").show();
+            $(".enter").text(`That's correct!`)
+            $(".enter2").text(`The answer is ${answer}`)
+            $(".submain").hide();
+            $(".showans2").hide();
+            yes.addEventListener("click",function(){
+              $(".input").hide();
+              $("#goback").show();
+              $(".submain").show();
+              $(".showans2").show();
+                secondquiz(test2,answer2);
+            })
+            no.addEventListener("click",function(){
+              $(".input").hide();
+              $(".leave").show();
+              $("#goback").show();
+            })  
+            }
+        else{
+            $(".showans2").addClass("showans")
+            $(".dec").text("Wrong Answer")
+        }
+    })
+    $(".submain").css("visibility","visible");
+    
+  }
+function secondquiz(test,answer){
+  $(".inputs").hide();
+  $(".inputs2").show();
+  $(".hintexts").text(test[0].text);
+  $(".hintn").text(test[0].hint)
+  var currentHintIndex2 = 1; 
+  currentHintIndex2
+  $(".next2").click(function() {
+    if (currentHintIndex2 < test.length) {
+      $(".hintexts").text(test[currentHintIndex2].text);
+      $(".hintn").text(test[currentHintIndex2].hint); 
+      currentHintIndex2++; 
+    } else {
+     
+      $(".hintexts").text("No more hints available.");
+    }
+  });
+  let submitBut = document.querySelector(".subBut2");
+  submitBut.addEventListener("click",function(){
+      let AnswerValue = $(".inputAnswer2").val();
+      let validation = AnswerValue.toLowerCase();
+      console.log(validation)
+      $(".inputAnswer2").val("")
+      if(validation == answer){
+          $(".showans2").addClass("showans")
+          $(".dec").text("Nice Job!");
+          $(".input").hide();
+          $(".leave").show();
+          $(".submain").hide();
+           
+            $(".enter").show();
+            $(".enter2").show(); 
+            $(".enter").text(`That's correct!`)
+            $(".enter2").text(`The answer is ${answer}`)
+          /*let prompt1 = prompt("do you want to continue?");
+          if(prompt1 == "yes"){
+            $(".submain").hide();
+            $(".showans2").hide();
+            firstQuiz(peter,"peter");
+            
+          }*/
+      }else{
+          $(".showans2").addClass("showans")
+          $(".dec").text("Wrong Answer")
+      }
+  })
+  $(".submain").css("visibility","visible");
+}
+
+
+
+$(document).ready(function() {
+  firstQuiz(peter,"peter",john,"john");
+   $("#easy").click(function(){
+    
+    $(".levelbut").hide();
+ 
+   })
+   $("#medium").click(function(){
+    
+    $(".levelbut").hide();
+   })
+   $("#hard").click(function(){
+    firstQuiz(paul,"paul",david,"david");
+    $(".levelbut").hide();
+   })
+
+  /* $(".t1").click(function(){
+    firstQuiz(peter,"peter");
+    $(".levelbut").hide();
+   })*/
+    //
+  });
+
+
+
